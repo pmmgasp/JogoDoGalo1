edf7cc0e32123d0fd98b1336619afc5048dc8d25
Pedro Gaspar <aluno23038@ipt.pt>
2023-07-01 20:06:02 +0100

    Página Inicial quase feita

--- client/jogodogalo/src/pages/Home.js
+++ client/jogodogalo/src/pages/Home.js
@@ -1,10 +1,24 @@
 import React from "react";
+import Img from "./Images/HS.jpg"
+import './Home.css';
 
 function Home() {
     return (
-        <div className="home">
-            <h1>Home</h1>
+        <>
+        <div className="home-title">
+            <h1>Jogo do Galo</h1>
         </div>
+        <div className="home-img">
+        <img src={Img} alt="Home Screen"/>
+        <img src={Img} alt="Home Screen"/>
+        <img src={Img} alt="Home Screen"/>
+        </div>
+        <div className="home-text">
+        O Jogo do Galo é jogado por dois jogadores num tabuleiro composto por uma matriz de três linhas por três colunas, podendo ser jogado sozinho ou com um amigo. 
+        Cada jogador joga alternadamente e o seu objetivo é conseguir três círculos "O" ou três "X" em linha, na horizontal, na vertical ou na diagonal. 
+        Ao mesmo tempo, quando possível, deverá tentar impedir o adversário de ganhar na próxima jogada.
+        </div>
+        </>
     )
 }
 
