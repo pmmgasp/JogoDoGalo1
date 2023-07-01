6beb3711e67cb88f8e89b99e9e8b9a017fbdfd13
Pedro Gaspar <aluno23038@ipt.pt>
2023-07-01 17:38:47 +0100

    Sidebar options added

--- client/jogodogalo/src/Components/Navbar.js
+++ client/jogodogalo/src/Components/Navbar.js
@@ -23,6 +23,17 @@ function Navbar() {
             <AiIcons.AiOutlineClose />
         </Link>
         </li>
+        {SidebarData.map((item, index) => {
+          return (
+            <li key={index} className={item.cName}>
+              <Link to={item.path}>
+                {item.icon}
+                <span>{item.title}</span>
+              </Link>
+            </li>
+          
+          )
+        })}
       </ul>
     </nav>
   </>
