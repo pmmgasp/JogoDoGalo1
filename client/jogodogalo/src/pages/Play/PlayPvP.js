0d550e44050eaa50a287167f0f29d57e129c0f9c
Pedro Gaspar <aluno23038@ipt.pt>
2023-07-02 16:51:02 +0100

    checkWinner funcional

--- client/jogodogalo/src/pages/Play/PlayPvP.js
+++ client/jogodogalo/src/pages/Play/PlayPvP.js
@@ -3,6 +3,17 @@ import {Board} from '../../components/Board'
 
 function PlayPvP() {
 
+  const WC = [
+    [0,1,2],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [3,4,5],
+    [6,7,8],
+    [0,4,8],
+    [2,4,6]
+  ]
+
   const [board, setBoard] = useState(Array(9).fill(null));
   const [xPlaying, setXPlaying] = useState(true)
  
@@ -18,8 +29,20 @@ function PlayPvP() {
       }
     })
 
+    checkWinner(updatedBoard);
     setBoard(updatedBoard);
     setXPlaying(!xPlaying);
+  }
+
+  const checkWinner =(board) =>{
+    for(const element of WC){
+      const[x,y,z] = element;
+
+      if (board[x] && board[x] === board[y] && board[y] === board[z]){
+        console.log(board[x])
+        return board[x]
+      }
+    }
   }
 
   return (
