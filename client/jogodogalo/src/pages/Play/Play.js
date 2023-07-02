faab195853f881b42aeeb511f3ad8ccb19c09ea7
Pedro Gaspar <aluno23038@ipt.pt>
2023-07-02 19:15:58 +0100

    Página "Jogar" concluida (por enquanto)

--- client/jogodogalo/src/pages/Play/Play.js
+++ client/jogodogalo/src/pages/Play/Play.js
@@ -1,9 +1,25 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import Img from "../../images/PvP.png"
+import Img1 from "../../images/PvM.png"
 
 function Play() {
+
+    let navigate = useNavigate(); 
+    const routeChange = () =>{ 
+      let path = `/playpvp`; 
+      navigate(path);  {/* Adiciona ao endereço da página o caminho da página pretendida */}
+    }
+
+    const routeChange1 = () =>{ 
+        let path = `/playpvm`; 
+        navigate(path);  {/* Adiciona ao endereço da página o caminho da página pretendida */}
+      }
+
     return (
-        <div className="play">
-            <h1>Play</h1>
+        <div className="about">
+            <button className="button2" onClick={routeChange}><img src={Img} alt="Github Pedro" className="play-img"/>Player vs Player</button> {/* Utilizador é redirecionado para a página Play ao pressionar o botão */}
+            <button className="button2" onClick={routeChange1}><img src={Img1} alt="Github Pedro" className="play-img"/>Player vs Machine</button>
         </div>
     )
 }
